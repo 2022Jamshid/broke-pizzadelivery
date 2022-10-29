daf86473e7815c6807c588ba8ea5929f42a3b5bf
2022Jamshid <116759540+2022Jamshid@users.noreply.github.com>
2022-10-29 11:04:33 +0300

    Update config.lua

--- broke-pizzadelivery/config.lua
+++ broke-pizzadelivery/config.lua
@@ -1,3 +1,3 @@
 Config = {}
-Config.Locale = 'en' -- available: en, fr
+Config.Locale = 'de' -- available: de, en, fr
 Config.ContactPhoto = 'CHAR_PROPERTY_BAR_UNBRANDED' -- For possible values, see: https://wiki.gtanet.work/index.php?title=Notification_Pictures
